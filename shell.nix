bb785b5bfc91200508b169296fe0edcff8a7120f
EuAndreh <eu@euandre.org>
2021-02-23 22:07:24 -0300

    Add shell.nix for usage with node2nix

---
+++ shell.nix
@@ -0,0 +1,8 @@
+# generate default.nix with:
+# $ node2nix -d -l
+(import ./default.nix { }).shell.overrideAttrs (oldAttrs: {
+  shellHook = ''
+    ${oldAttrs.shellHook}
+    ln -sf $NODE_PATH
+  ''
+});
